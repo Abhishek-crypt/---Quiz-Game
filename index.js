247513c2fcb7f460f24c33f2e993434ad5247e32
Abhi2345 <abhishekjh18@gmail.com>
2020-12-04 06:03:23 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+//The Quiz Game
+var chalk=require('chalk');
+var takeinput=require('readline-sync');
+var q1={
+  queOne:"Well do you know my name?",
+  ansOne:"Jacky Shaw"
+}
+var q2={
+  queTwo:"Guess my age if I am millennial?",
+  ansOne:22
+}
+var q3={
+  queThree:"What's my favourite book?",
+  ansThree:"Power of Now"
+}
+var q4={
+  queFour:"Whom do I admire the most?",
+  ansFour:"Vivekananda"
+}
+var q5={
+  queFive:"What is my favourite no.?",
+  ansFive:1
+}
+var q6={
+  queSix:"Where do I live?",
+  ansSix:"Kolkata"
+}
+var q7={
+  queSeven:"I prefer Green Tea or Coffee?",
+  ansSeven:"Green Tea"
+}
+var q8={
+  queEight:"Who is my favourite superhero?",
+  ansEight:"Iron Man"
+}
+var q9={
+  queNine:"Who is my favourite cartoon character?",
+  ansNine:"Doraemon"
+}
+var q10={
+  queTen:"What is my goal?",
+  ansTen:"Product Based Company"
+}
+var quizList=[q1,q2,q3,q4,q5,q6,q7,q8,q9,q10];
